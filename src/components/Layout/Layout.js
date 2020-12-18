1dc5a3b4785f824dc6dabe27f724b7986496ed26
MartinHage <LabsDashboard20@gmail.com>
2020-12-18 14:59:53 +0100

    added mobile navigation sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import "./Layout.css";
 
 const Layout = (props) => (
   <>
     <Toolbar />
+    <SideDrawer />
     <main className="Content">{props.children}</main>
   </>
 );
