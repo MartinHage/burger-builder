a8aa2965b513e28e62f8cd074c5028831587bfef
MartinHage <hagemartins@gmail.com>
2021-01-11 14:59:44 +0100

    added fetching of past orders

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -5,7 +5,7 @@ import "./NavigationItem.css";
 const NavigationItem = (props) => {
   return (
     <li className="NavigationItem">
-      <NavLink to={props.link} exact activeClassName="active">
+      <NavLink to={props.link} exact={props.exact} activeClassName="active">
         {props.children}
       </NavLink>
     </li>
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,28 @@
 import React from "react";
 import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 const Orders = () => {
+  const [orders, setOrders] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    axios
+      .get("/orders")
+      .then((res) => {
+        console.log(res);
+        const fetchedOrders = [];
+        for (const key in res.data) {
+          fetchedOrders.push({ ...res.data[key], id: key });
+        }
+        setOrders(fetchedOrders);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+      });
+  }, []);
   return (
     <div>
       <Order /> <Order />
@@ -9,4 +30,4 @@ const Orders = () => {
   );
 };
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
